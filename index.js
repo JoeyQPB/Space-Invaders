feb01228897c7cbe60f21e72204aa00f2be94acc
Joey Quadros <joey_qpb@hotmail.com>
2023-01-24 13:13:50 -0300

    feat: add move player

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ class Player {
       y: 0,
     };
 
+    this.rotation = 0;
+
     const image = new Image();
     image.src = "./img/spaceship.png";
 
@@ -30,25 +32,99 @@ class Player {
   draw() {
     // c.fillStyle = "red";
     // c.fillRect(this.position.x, this.position.y, this.width, this.height);
-    if (this.image)
-      c.drawImage(
-        this.image,
-        this.position.x,
-        this.position.y,
-        this.width,
-        this.height
-      );
+
+    c.save();
+    c.translate(
+      player.position.x + player.width / 2,
+      player.position.y + player.height / 2
+    );
+    c.rotate(this.rotation);
+
+    c.translate(
+      -player.position.x - player.width / 2,
+      -player.position.y - player.height / 2
+    );
+
+    c.drawImage(
+      this.image,
+      this.position.x,
+      this.position.y,
+      this.width,
+      this.height
+    );
+
+    c.restore();
+  }
+
+  update() {
+    if (this.image) {
+      this.draw();
+      this.position.x += this.velocity.x;
+    }
   }
 }
 
 const player = new Player();
+const keys = {
+  a: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+  space: {
+    pressed: false,
+  },
+};
 
 function animate() {
   requestAnimationFrame(animate);
   c.fillRect(0, 0, canvas.width, canvas.height);
-  c.fillStyle = "balck";
+  c.fillStyle = "black";
 
-  player.draw();
+  player.update();
+
+  if (keys.a.pressed && player.position.x >= 0) {
+    player.velocity.x = -7;
+    player.rotation = -0.15;
+  } else if (
+    keys.d.pressed &&
+    player.position.x + player.width <= canvas.width
+  ) {
+    player.velocity.x = 7;
+    player.rotation = 0.15;
+  } else {
+    player.velocity.x = 0;
+    player.rotation = 0;
+  }
 }
 
 animate();
+
+addEventListener("keydown", ({ key }) => {
+  switch (key) {
+    case "a":
+      keys.a.pressed = true;
+      break;
+    case "d":
+      keys.d.pressed = true;
+      break;
+    case " ":
+      console.log("space");
+      break;
+  }
+});
+
+addEventListener("keyup", ({ key }) => {
+  switch (key) {
+    case "a":
+      keys.a.pressed = false;
+      break;
+    case "d":
+      keys.d.pressed = false;
+      break;
+    case " ":
+      console.log("space");
+      break;
+  }
+});
