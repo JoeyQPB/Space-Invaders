3d6f9b2649b0b5e30070989facd29bcdcdfbaa7b
Joey Quadros <joey_qpb@hotmail.com>
2023-01-24 15:59:52 -0300

    feat: created an invader

--- index.js
+++ index.js
@@ -64,6 +64,48 @@ class Player {
   }
 }
 
+class Invader {
+  constructor() {
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
+
+    const image = new Image();
+    image.src = "./img/invader.png";
+
+    image.onload = () => {
+      const scale = 1;
+      this.image = image;
+      this.width = image.width * scale;
+      this.height = image.height * scale;
+
+      this.position = {
+        x: canvas.width / 2 - this.width / 2,
+        y: canvas.height / 2,
+      };
+    };
+  }
+
+  draw() {
+    c.drawImage(
+      this.image,
+      this.position.x,
+      this.position.y,
+      this.width,
+      this.height
+    );
+  }
+
+  update() {
+    if (this.image) {
+      this.draw();
+      this.position.x += this.velocity.x;
+      this.position.y += this.velocity.y;
+    }
+  }
+}
+
 class Projectile {
   constructor({ position, velocity }) {
     this.position = position;
@@ -89,6 +131,7 @@ class Projectile {
 
 const player = new Player();
 const projectiles = [];
+const invader = new Invader();
 const keys = {
   a: {
     pressed: false,
@@ -104,6 +147,7 @@ const keys = {
 function animate() {
   requestAnimationFrame(animate);
   c.fillRect(0, 0, canvas.width, canvas.height);
+  invader.update();
   projectiles.forEach((projectile, i) => {
     if (projectile.position.y + projectile.radius <= 0) {
       setTimeout(() => {
