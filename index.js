df5a48a6c7609223561af56984a7ac3d5ceefb3c
Joey Quadros <joey_qpb@hotmail.com>
2023-01-25 13:06:33 -0300

    feat: create the background starts

--- index.js
+++ index.js
@@ -188,13 +188,13 @@ class Projectile {
 }
 
 class Particle {
-  constructor({ position, velocity, radius, color }) {
+  constructor({ position, velocity, radius, color, fades }) {
     this.position = position;
     this.velocity = velocity;
-
     this.radius = radius;
     this.color = color;
     this.opacity = 1;
+    this.fades = fades;
   }
 
   draw() {
@@ -212,7 +212,8 @@ class Particle {
     this.draw();
     this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
-    this.opacity -= 0.01;
+
+    if (this.fades) this.opacity -= 0.01;
   }
 }
 
@@ -258,7 +259,24 @@ const keys = {
 let frames = 0;
 let randomInterval = Math.floor(Math.random() * 500 + 500);
 
-function createParticles({ object, color }) {
+for (let i = 0; i < 100; i++) {
+  particles.push(
+    new Particle({
+      position: {
+        x: Math.random() * canvas.width,
+        y: Math.random() * canvas.height,
+      },
+      velocity: {
+        x: 0,
+        y: 0.35,
+      },
+      radius: Math.random() * 3,
+      color: "white",
+    })
+  );
+}
+
+function createParticles({ object, color, fades }) {
   for (let i = 0; i < 15; i++) {
     particles.push(
       new Particle({
@@ -274,6 +292,7 @@ function createParticles({ object, color }) {
         color: color || "#BAA0DE",
         // color: "#BAA0DE",
         // color: "yellow",
+        fades,
       })
     );
   }
@@ -283,6 +302,11 @@ function animate() {
   requestAnimationFrame(animate);
   c.fillRect(0, 0, canvas.width, canvas.height);
   particles.forEach((particle, index) => {
+    if (particle.position.y - particle.radius >= canvas.height) {
+      particle.position.x = Math.random() * canvas.width;
+      particle.position.y = -particle.radius;
+    }
+
     if (particle.opacity <= 0) {
       setTimeout(() => {
         particles.splice(index, 1);
@@ -319,7 +343,7 @@ function animate() {
             );
 
             if (invaderFound && projectileFound) {
-              createParticles({ object: invader });
+              createParticles({ object: invader, fades: true });
               grid.invaders.splice(i, 1);
               projectiles.splice(j, 1);
 
@@ -363,7 +387,7 @@ function animate() {
         invaderProjectiles.splice(index, 1);
       }, 0);
       console.log("Game Over");
-      createParticles({ object: player, color: "white" });
+      createParticles({ object: player, color: "yellow", fades: true });
     }
   });
 
